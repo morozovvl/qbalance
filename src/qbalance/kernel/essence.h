60a836f67baeb360d142c9c31568acedd5834d1f
Vladimir A. Morozov <MorozovVladimir@mail.ru>
2016-04-22 11:33:11 +0300

    Исправления

--- src/qbalance/kernel/essence.h
+++ src/qbalance/kernel/essence.h
@@ -43,7 +43,6 @@ Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 #include "../gui/formgrid.h"
 #include "../gui/tableview.h"
 
-
 class TApplication;
 
 struct urlId {
