0c1546c5cbdb429026477c46eb49bd3cdee1fda0
Vladimir A. Morozov <MorozovVladimir@mail.ru>
2018-11-27 20:58:11 +0300

    Исправления

---
+++ src/qbalance/emailclient/emailclient.cpp
@@ -0,0 +1,106 @@
+/************************************************************************************************************
+Copyright (C) Morozov Vladimir Aleksandrovich
+MorozovVladimir@mail.ru
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*************************************************************************************************************/
+
+#include "emailclient.h"
+#include "../../SMTPEmail/src/SmtpMime"
+#include "../../SMTPEmail/src/smtpclient.h"
+#include "../../SMTPEmail/src/mimemessage.h"
+
+
+EMailClient::EMailClient(QObject *parent) :
+    QObject(parent)
+{
+}
+
+
+EMailClient::~EMailClient()
+{
+
+}
+
+
+bool EMailClient::open()
+{
+    return true;
+}
+
+
+void EMailClient::close()
+{
+
+}
+
+
+int EMailClient::sendMail()
+{
+    SmtpClient* smtp = new SmtpClient("smtp.gmail.com", 465, SmtpClient::SslConnection);
+
+    // We need to set the username (your email address) and password
+    // for smtp authentification.
+
+    smtp->setUser("ip5540726@gmail.com");
+    smtp->setPassword("mva10031974");
+
+/*
+    // Now we create a MimeMessage object. This is the email.
+
+    MimeMessage message;
+
+    EmailAddress sender("ip5540726@gmail.com", "Your Name");
+    message.setSender(&sender);
+
+    EmailAddress to("MorozovVladimir@mail.ru", "Recipient's Name");
+    message.addRecipient(&to);
+
+    message.setSubject("mailclient for Qt - Demo");
+
+    // Now add some text to the email.
+    // First we create a MimeText object.
+
+    MimeText text;
+
+    text.setText("Hi,\nThis is a simple email message.\n");
+
+    // Now add it to the mail
+
+    message.addPart(&text);
+
+    // Now we can send the mail
+
+    if (!smtp->connectToHost()) {
+        qDebug() << "Failed to connect to host!" << endl;
+        return -1;
+    }
+
+    if (!smtp->login()) {
+        qDebug() << "Failed to login!" << endl;
+        return -2;
+    }
+
+    if (!smtp->sendMail(message)) {
+        qDebug() << "Failed to send mail!" << endl;
+        return -3;
+    }
+*/
+    smtp->quit();
+
+    delete smtp;
+
+    return 0;
+}
---
+++ src/qbalance/emailclient/emailclient.pro
@@ -0,0 +1,24 @@
+CONFIG += plugin
+
+unix:TARGET = ../../../plugins/emailclient
+win32:TARGET = ../../../../plugins/emailclient
+
+TEMPLATE = lib
+
+DEPENDPATH += .
+INCLUDEPATH += .
+
+HEADERS += emailclient.h \
+            emailclientplugin.h
+SOURCES += emailclient.cpp \
+            emailclientplugin.cpp
+
+unix:MOC_DIR = ../../../.moc
+win32:MOC_DIR = ../../../.moc
+
+unix:OBJECTS_DIR = ../../../.obj
+win32:OBJECTS_DIR = ../../../.obj
+
+unix:RCC_DIR = ../..
+win32:RCC_DIR = ../..
+
